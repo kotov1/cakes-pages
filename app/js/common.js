eda48a7eb8a599091f07253af3901a63e14d8fb7
Petr Petukhov <kotov0991@gmail.com>
2017-11-27 00:55:26 +0300

    all sections

--- app/js/common.js
+++ app/js/common.js
@@ -1,25 +1,5 @@
 $(function() {
 
-	/*Masonry in blog.html*/
-	/* https://masonry.desandro.com */
-
-	// $('.shop_cards').masonry({
-	// 	itemSelector: '.shop_card',
-	// 	gutter: 20
-	// });
-
-	$('.shop_cards').masonry({
-		itemSelector: '.shop_card',
-		gutter: 20,
-		percentPosition: true
-	});
-
-
-
-
-
-
-
 
 
 	$('.companyBlog_wrap').masonry({
@@ -29,7 +9,7 @@ $(function() {
 
 
 
-	$(".slider").slick({
+	$(".slider_main").slick({
 		// normal options...
 		infinite: true,
 		arrows: true,
@@ -45,5 +25,51 @@ $(function() {
 
 		}]
 	});
+
+	$(".slider_about").slick({
+		infinite: true,
+		arrows: true,
+		// autoplay: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplaySpeed: 5000,
+
+		responsive: [
+
+		{
+		breakpoint: 1200,
+		settings: {
+			slidesToShow: 2
+			}
+		},
+
+		{
+		breakpoint: 768,
+		settings: {
+			slidesToShow: 1
+			}
+		},
+
+		{
+		breakpoint: 576,
+		settings: {
+			slidesToShow: 1,
+			adaptiveHeight: true,
+			arrows: false
+			}
+
+		}]
+	});
+
+
+	//Плавный скролл к якорю
+	$(".scrollDown").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+		top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 500);
+	});
+
+
 
 });
